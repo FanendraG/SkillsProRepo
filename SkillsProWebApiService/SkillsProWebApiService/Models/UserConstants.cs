8cc749e4dbe92da4388f595f6b6194416bc19430
FanendraG <fanendraa@gmail.com>
2023-01-10 20:54:15 -0600

    removed secrets

--- SkillsProWebApiService/SkillsProWebApiService/Models/UserConstants.cs
+++ SkillsProWebApiService/SkillsProWebApiService/Models/UserConstants.cs
@@ -7,10 +7,6 @@ namespace SkillsProWebApiService.Models
 {
     public class UserConstants
     {
-        public static List<UserModel> Users = new List<UserModel>()
-        {
-            new UserModel() { Username = "fa.ganti", EmailAddress = "fa.ganti@reply.com", Password = "Pa$$word_Admin", GivenName = "Fanendra", Surname = "Ganti", Role = "Administrator" },
-            new UserModel() { Username = "elyse_seller", EmailAddress = "elyse.seller@email.com", Password = "Pa$$word_User", GivenName = "Elyse", Surname = "Lambert", Role = "Seller" },
-        };
+        
     }
 }
